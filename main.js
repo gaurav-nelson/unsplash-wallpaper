5e635991c891d78a7bd865165872ae6761e3d685
Gaurav Nelson <gaurav.nelson@outlook.com>
2020-12-16 10:50:53 +1000

    Change wallpaper engine

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const { app, BrowserWindow } = require("electron");
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 576,
     webPreferences: {
       nodeIntegration: true,
     },
